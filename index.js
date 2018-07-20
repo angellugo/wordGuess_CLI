cbdec0d252352d14636d8f45b7da26af28dbf5d9
Angel Lugo <this@angellugo.com>
2018-07-20 12:54:31 -0700

    Working version of the game

--- index.js
+++ index.js
@@ -1,94 +1,77 @@
 const Word = require ('./word.js');
+const colors = require ('colors');
 
 const stdin = process.stdin;
-const word = new Word('who');
+const word = new Word ('Who');
+let missedLetters = [];
 
-// without this, we would only get streams once enter is pressed
-stdin.setRawMode (true);
+stdin.setRawMode (true); // to get key presses without enter being pressed
+stdin.resume (); // resume stdin in the parent process (node app won't quit all by itself unless an error or process.exit() happens)
+stdin.setEncoding ('utf8'); // so we don't get binary
 
-// resume stdin in the parent process (node app won't quit all by itself
-// unless an error or process.exit() happens)
-stdin.resume ();
-
-// i don't want binary, do you?
-stdin.setEncoding ('utf8');
+console.log ('\n' + word.toString ());
+process.stdout.write ('Guess a letter: '); //use stdout so that the letter that follows shows in the same line
 
 // on any data into stdin
 stdin.on ('data', function (key) {
-  // ctrl-c ( u0003 ), ESC (u001B)
-  if (key === '\u0003' || key === '\u001B') {
+  const ctrl_c = '\u0003'; // the ctrl-c key is u0003
+  const esc = '\u001B'; // the ESC key is u001B
+
+  const keyIsAlphabetic = /(^[a-zA-Z]$)/.test (key);
+
+  if (key === ctrl_c || key === esc) {
     process.exit ();
-  }
-  if (/([a-zA-Z])/.test (key)) {
+  } else if (keyIsAlphabetic) {
     console.log (key);
     play (key);
+  } else {
+    console.log ('Invalid key press'.bgRed);
+    process.stdout.write ('Please try again... Guess a letter: '); //use stdout so that the letter that follows shows in the same line
   }
 });
 
-function play (key) {
-  word.guess (key);
-  console.log (word.toString ());
+function play (letter) {
+  if (letterHasAlreadyBeenGuessed (letter)) {
+    console.log (
+      colors.yellow ('You already guessed ' + letter + '. Try again...')
+    );
+  } else if (word.guess (letter)) {
+    console.log ("You're right!".green);
+  } else {
+    console.log ('Wrong.Try again!'.red);
+    missedLetters.push (letter);
+  }
+
+  console.log ('\nMissed letters: ' + missedLetters.join (',').yellow);
+  console.log ('\n' + word);
+
   if (wordHasBeenGuessed ()) {
-    console.log ('you won');
-    process.exit();
+    console.log ('\nYou won!!!'.rainbow);
+    process.exit ();
+  } else if (missedLetters.length === word.word.length) {
+    console.log ('You lost...'.blue);
+    process.exit (1);
+  } else {
+    const guessesRemaining = word.word.length - missedLetters.length;
+    if (guessesRemaining === 1) {
+      console.log ('You have 1 guess remaining...');
+    } else {
+      console.log ('You have ' + guessesRemaining + ' guesses remaining...');
+    }
+
+    process.stdout.write ('Guess a letter: '); //use stdout so that the letter that follows shows in the same line
   }
 }
 
-// require ('keyscan').make_scanner (caught => {
-//   console.log ('Caught ' + caught.parsed);
-//   while (caught.parsed !== 'escape') {
-//     console.log ('hello');
-//   }
-//   const inquirer = require ('inquirer');
-//   inquirer
-//     .prompt ([
-//       {
-//         type: 'input',
-//         message: 'What is your name?',
-//         name: 'name',
-//       },
-//     ])
-//     .then (answers => {
-//       console.log ('answers.name', answers.name);
-//     });
-// });
+function letterHasAlreadyBeenGuessed (letter) {
+  return (
+    missedLetters.indexOf (letter.toUpperCase ()) > -1 ||
+    missedLetters.indexOf (letter.toLowerCase ()) > -1 ||
+    word.toString ().indexOf (letter.toUpperCase ()) > -1 ||
+    word.toString ().indexOf (letter.toLowerCase ()) > -1
+  );
+}
 
-// const inquirer = require('inquirer');
-// inquirer.prompt([{
-//     type: 'input',
-//     message: 'What is your name?',
-//     name: 'name',
-//   }]).then(answers => {
-//     console.log("answers.name", answers.name);
-
-// });
-
-// const word = new Word('who');
-
-// console.log(word.toString());
-// if (wordHasBeenGuessed()){
-//     console.log("you won");
-
-// }
-// word.guess('w');
-// console.log(word.toString());
-// if (wordHasBeenGuessed()){
-//     console.log("you won");
-
-// }
-// word.guess('h');
-// console.log(word.toString());
-
-// if (wordHasBeenGuessed()){
-//     console.log("you won");
-
-// }
-// word.guess('o');
-// console.log(word.toString());
-// if (wordHasBeenGuessed()){
-//     console.log("you won");
-
-// }
-function wordHasBeenGuessed() {
-    return (word.toString().indexOf('■') === -1);
+function wordHasBeenGuessed () {
+  return word.toString ().indexOf ('■') === -1;
 }
