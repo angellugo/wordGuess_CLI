91d4f5cb5ee559466e3e2507d5a102b0dbe7b5ef
Angel Lugo <this@angellugo.com>
2018-07-21 16:05:17 -0700

    Added random-words and inquirer packages and neede code

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const Word = require ('./word.js');
 const colors = require ('colors');
 const inquirer = require ('inquirer');
+const randomWord = require ('random-words');
 
-const stdin = process.stdin;
-const word = new Word ('Who');
+let word = new Word (randomWord());
 let missedLetters = [];
 
-stdin.setRawMode (true); // to get key presses without enter being pressed
-stdin.resume (); // resume stdin in the parent process (node app won't quit all by itself unless an error or process.exit() happens)
-stdin.setEncoding ('utf8'); // so we don't get binary
+let stdinRawMode = true;
+const stdin = process.stdin;
+startGame();
 
-console.log ('\n' + word.toString ());
-process.stdout.write ('Guess a letter: '); //use stdout so that the letter that follows shows in the same line
+function startGame() {
+  stdin.setRawMode (stdinRawMode); // to get key presses without enter being pressed
+  stdin.resume (); // resume stdin in the parent process (node app won't quit all by itself unless an error or process.exit() happens)
+  stdin.setEncoding ('utf8'); // so we don't get binary
 
-// on any data into stdin
-stdin.on ('data', function (key) {
+  console.log ('\n' + word.toString ());
+  process.stdout.write ('Guess a letter: '); //use stdout so that the letter that follows shows in the same line
+
+
+  // on any data into stdin
+  stdin.on ('data', listenForKeypress);
+}
+
+function listenForKeypress(key) {
+  stdin.setRawMode (stdinRawMode);
+  if (!stdin.isRaw) return; // do this for when inquirer is working
+
   const ctrl_c = '\u0003'; // the ctrl-c key is u0003
   const esc = '\u001B'; // the ESC key is u001B
 
@@ -29,8 +41,7 @@ stdin.on ('data', function (key) {
     console.log ('Invalid key press'.bgRed);
     process.stdout.write ('Please try again... Guess a letter: '); //use stdout so that the letter that follows shows in the same line
   }
-});
-
+};
 function play (letter) {
   if (letterHasAlreadyBeenGuessed (letter)) {
     console.log (
@@ -47,12 +58,12 @@ function play (letter) {
 
   if (wordHasBeenGuessed ()) {
     console.log ('\n' + word);
-    console.log ('\nYou won!!!'.rainbow);
-    process.exit ();
+    console.log ('\nYou won!!!\n'.rainbow);
+    askToPlayAgain ();
   } else if (missedLetters.length === word.length) {
     console.log ('You lost...'.blue);
-    console.log(colors.blue('The word was '+ word.reveal()));
-    process.exit (1);
+    console.log (colors.blue ('The word was ' + word.reveal ()));
+    askToPlayAgain ();
   } else {
     const guessesRemaining = word.length - missedLetters.length;
     if (guessesRemaining === 1) {
@@ -77,4 +88,29 @@ function letterHasAlreadyBeenGuessed (letter) {
 
 function wordHasBeenGuessed () {
   return word.toString ().indexOf ('■') === -1;
+}
+
+function askToPlayAgain (params) {
+  stdinRawMode = false;
+
+  inquirer
+    .prompt ([
+      {
+        type: 'confirm',
+        message: 'Would you like to play again?',
+        name: 'playAgain',
+        default: true,
+      },
+    ])
+    .then (function (inquirerResponse) {
+      if (inquirerResponse.playAgain) {
+        stdinRawMode = true;
+        stdin.removeListener('data', listenForKeypress);
+        word = new Word (randomWord());
+        missedLetters = [];
+        startGame();
+      } else {
+        process.exit ();
+      }
+    });
 }
